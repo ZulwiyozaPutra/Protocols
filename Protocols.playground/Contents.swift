0ffadd5618fb7800a2b99730fd2cb218339a3aee
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-09-04 09:37:45 -0700

    third

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -34,3 +34,87 @@ struct Person: UserType {
 }
 
 let somePerson = Person(name: "Zulwiyoza Putra", age: 21)
+
+/***************************************************************************/
+
+import Foundation
+
+enum EmployeeType {
+    case Manager
+    case NotManager
+}
+
+class Employee {
+    
+    let name: String
+    let address: String
+    let startDate: NSDate
+    let type: EmployeeType
+    
+    var department: String?
+    var reportsTo: String?
+    
+    init(fullName: String, employeeAddress: String, employeeStartDate: NSDate, employeeType: EmployeeType) {
+        self.name = fullName
+        self.address = employeeAddress
+        self.startDate = employeeStartDate
+        self.type = employeeType
+    }
+    
+    func pay() -> (basPay: Double, benefits: Double, deductions: Double, vacationTime: Int) {
+        return (0, 0, 0, 0)
+    }
+}
+
+func payEmployee(employee: Employee) -> Void {
+    let paycheck = employee.pay()
+}
+
+class HourlyEmployee: Employee {
+    var hourlyWage: Double = 15.00
+    var hoursWorked: Double = 0.0
+    var availableVacaion: Int = 0
+    
+    override func pay() -> (basPay: Double, benefits: Double, deductions: Double, vacationTime: Int) {
+        return (hourlyWage * hoursWorked, 0, 0, availableVacaion )
+    }
+}
+
+//let hourlyEmployee = HourlyEmployee(fullName: "Zulwiyoz", employeeAddress: <#T##String#>, employeeStartDate: <#T##NSDate#>, employeeType: <#T##EmployeeType#>)
+
+
+
+enum Level {
+    case good
+    case bad
+}
+
+enum Status {
+    case excellent
+    case okay
+    case bad
+}
+
+
+
+
+
+func opportunity(gpa: Double?, yokiQuality: Level) -> Status {
+    if gpa == nil {
+        switch yokiQuality {
+        case Level.good:
+            return Status.okay
+        case Level.bad:
+            return Status.bad
+        }
+    } else {
+        switch yokiQuality {
+        case Level.good:
+            return Status.excellent
+        case Level.bad:
+            return Status.bad
+        }
+    }
+}
+
+opportunity(3.2, yokiQuality: Level.good)
