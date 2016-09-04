b66a25cdec3e4f91728c839b1bd1d8290eec3ab3
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-09-04 15:21:05 -0700

    5

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -39,6 +39,10 @@ let somePerson = Person(name: "Zulwiyoza Putra", age: 21)
 
 import Foundation
 
+protocol Payable {
+    func pay() -> (basePay: Double, benefits: Double, deductions: Double, vacationTime: Int)
+}
+
 enum EmployeeType {
     case Manager
     case NotManager
@@ -61,26 +65,121 @@ class Employee {
         self.type = employeeType
     }
     
-    func pay() -> (basPay: Double, benefits: Double, deductions: Double, vacationTime: Int) {
-        return (0, 0, 0, 0)
-    }
+//    func pay() -> (basePay: Double, benefits: Double, deduction: Double, vacationTime: Int) {
+//        return (0, 0, 0, 0)
+//    }
+
 }
 
-func payEmployee(employee: Employee) -> Void {
+
+func payEmployee(employee: Payable) -> Void {
     let paycheck = employee.pay()
+    print(paycheck)
 }
 
-class HourlyEmployee: Employee {
+class HourlyEmployee: Employee, Payable {
     var hourlyWage: Double = 15.00
-    var hoursWorked: Double = 0.0
+    var hoursWorked: Double = 10
     var availableVacaion: Int = 0
     
-    override func pay() -> (basPay: Double, benefits: Double, deductions: Double, vacationTime: Int) {
-        return (hourlyWage * hoursWorked, 0, 0, availableVacaion )
+    func pay() -> (basePay: Double, benefits: Double, deductions: Double, vacationTime: Int) {
+        return (hourlyWage * hoursWorked, 0, 0, availableVacaion)
     }
 }
 
-//let hourlyEmployee = HourlyEmployee(fullName: "Zulwiyoz", employeeAddress: <#T##String#>, employeeStartDate: <#T##NSDate#>, employeeType: <#T##EmployeeType#>)
+let hourlyEmployee = HourlyEmployee(fullName: "Zulwiyoza Putra", employeeAddress: "no", employeeStartDate: NSDate(), employeeType: EmployeeType.NotManager)
+payEmployee(hourlyEmployee)
+
+let employee = Employee(fullName: "Zulwiyoza Putra", employeeAddress: "some address", employeeStartDate: NSDate(), employeeType: EmployeeType.NotManager)
+
+/******************************************************/
+
+//PROTOCOL CHALLENGE
+
+// Declare protocol here
+
+protocol ColorSwitchable {
+    func switchColor(color: Color)
+}
+
+enum LightState {
+    case On
+    case Off
+}
+
+enum Color {
+    case RGB(Double, Double, Double, Double)
+    case HSB(Double, Double, Double, Double)
+}
+
+class WifiLamp: ColorSwitchable {
+    var state: LightState
+    var color: Color
+    
+    init() {
+        self.state = LightState.On
+        self.color = Color.RGB(0,0,0,0)
+    }
+    
+    func switchColor(color: Color) {
+        self.color = Color.RGB(121, 0, 0, 0)
+    }
+}
+
+
+/******************************************************/
+
+
+protocol Blendable {
+    func blend()
+}
+
+class Fruit: Blendable {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func blend() {
+        print("I'm a mush")
+    }
+}
+
+class Dairy {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Cheese: Dairy {}
+
+class Milk: Dairy, Blendable {
+    func blend() {
+        print("I'm stil milk after blended")
+    }
+}
+
+func makeSmoothie(ingredients: [Blendable]) {
+    for ingredient in ingredients {
+        ingredient.blend()
+        print("\(ingredient) is blended")
+    }
+}
+
+let strawberry = Fruit(name: "Strawberry")
+let cheddar = Cheese(name: "Cheddar")
+let chocolateMilk = Milk(name: "Chocolate")
+
+let ingredients: [Blendable] = [strawberry, chocolateMilk]
+
+makeSmoothie(ingredients)
+
+
+
+
 
 
 
@@ -94,10 +193,6 @@ enum Status {
     case okay
     case bad
 }
-
-
-
-
 
 func opportunity(gpa: Double?, yokiQuality: Level) -> Status {
     if gpa == nil {
